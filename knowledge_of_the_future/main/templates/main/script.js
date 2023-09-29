de4f7d5ff0b9f6849041acf138027a244bdac18e
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2023-09-29 16:33:05 +0300

    Finished feature with nav items oppacity

--- knowledge_of_the_future/main/templates/main/script.js
+++ knowledge_of_the_future/main/templates/main/script.js
@@ -3,36 +3,35 @@
 document.addEventListener("DOMContentLoaded", function() {
     // NavBar
     
-    const navBar = document.querySelector('header'),
-          menuItems = document.querySelectorAll('header li');
+    // const navBar = document.querySelector('header'),
+    //       menuItems = document.querySelectorAll('header li');
 
-    function handleMouseOver() {
-        menuItems.forEach(item => {
-            const firstLetter = item.querySelector('.first-letter'),
-                  restOfText = item.querySelector('.rest-of-text');
-            console.log(firstLetter);
-            console.log(restOfText); 
-            if (firstLetter && restOfText) {
-                restOfText.style.display = 'inline';
-            }
-        });
-    }
+    // function handleMouseOver() {
+    //     menuItems.forEach(item => {
+    //         const firstLetter = item.querySelector('.first-letter'),
+    //               restOfText = item.querySelector('.rest-of-text');
+    //         console.log(firstLetter);
+    //         console.log(restOfText); 
+    //         if (firstLetter && restOfText) {
+    //             restOfText.style.display = 'inline';
+    //         }
+    //     });
+    // }
 
-    function handleMouseOut() {
-        menuItems.forEach(item => {
-            const firstLetter = item.querySelector('.first-letter'),
-                  restOfText = item.querySelector('.rest-of-text');
+    // function handleMouseOut() {
+    //     menuItems.forEach(item => {
+    //         const firstLetter = item.querySelector('.first-letter'),
+    //               restOfText = item.querySelector('.rest-of-text');
 
-            if (firstLetter && restOfText) {
-                restOfText.style.display = 'none';
-            }
-        });
-    }
+    //         if (firstLetter && restOfText) {
+    //             restOfText.style.display = 'none';
+    //         }
+    //     });
+    // }
 
-    handleMouseOut();
-    navBar.addEventListener('mouseover', handleMouseOver);
-    navBar.addEventListener('mouseout', handleMouseOut);
+    // handleMouseOut();
+    // navBar.addEventListener('mouseover', handleMouseOver);
+    // navBar.addEventListener('mouseout', handleMouseOut);
     
 
- //da
   });

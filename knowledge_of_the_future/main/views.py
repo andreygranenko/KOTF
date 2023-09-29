9eeef291a7e37f68f1f1b028c76a5e3675d8476a
Teodors Chaly <iteo.chaly@gmail.com>
2023-09-29 15:37:54 +0300

    Adding first page.

--- knowledge_of_the_future/main/views.py
+++ knowledge_of_the_future/main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+    return render(request,'main/index.html' )

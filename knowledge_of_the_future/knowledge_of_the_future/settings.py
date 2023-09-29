16d5caa68711cfbae7bbd7ea7fbe1b4d0b2779b8
Teodors Chaly <iteo.chaly@gmail.com>
2023-09-29 16:58:00 +0300

    Connecting Static files (CSS) with Django.

--- knowledge_of_the_future/knowledge_of_the_future/settings.py
+++ knowledge_of_the_future/knowledge_of_the_future/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 # Application definition
 

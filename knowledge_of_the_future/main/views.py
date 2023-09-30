38ea02bf170e5f055a247fc22f3213364d1c2c1a
Teodors Chaly <iteo.chaly@gmail.com>
2023-09-30 10:48:20 +0300

    URL in header

--- knowledge_of_the_future/main/views.py
+++ knowledge_of_the_future/main/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
     return render(request,'main/index.html' )
+
+def courses(request):
+    return render(request,'main/courses.html' )
